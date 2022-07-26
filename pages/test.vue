3d58fbf77fa4aa96573072e8cb66e4e3f0844248
kevalkennect <keval.panchal@kennect.io>
2022-07-26 12:26:57 +0530

    Formating Date Using Moment js

---
+++ pages/test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <input type="text" v-model="inputDate" placeholder="Enter A Date" />
+    <button @click="formatDate(inputDate)">Format Date</button>
+    <div class="formatedDate" v-for="date in formatedDate" :key="date">
+      {{ date }}
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  data() {
+    return {
+      inputDate: "",
+      formatFrom: [
+        "DD/MM",
+        "DD/MM/YYYY",
+        "MM/YYYY",
+        "YYYY",
+        "MM",
+        "DD",
+        "YYYY/MM",
+      ],
+      formatedDate: [],
+    };
+  },
+  methods: {
+    formatDate(date) {
+      if (date == "" || null || undefined) return;
+      this.formatedDate = [];
+      for (let i of this.formatFrom) {
+        this.formatedDate.push(moment(date).format(i));
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
